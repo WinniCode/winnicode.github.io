6c19fb0c8da935ad2450feec5e215c96f6d077e9
Chris <109357782+WinniCode@users.noreply.github.com>
2026-05-13 13:16:30 +0100

    Update main.js

--- jspf/main.js
+++ jspf/main.js
@@ -16,7 +16,8 @@ document.addEventListener("DOMContentLoaded", () => {
   
   const aboutTrigger = document.querySelector('#about-trigger');
   const aboutWrapper = document.querySelector('.about-wrapper');
-  const aboutContent = "Hello! My name's Chris, I'm a Computational Biologist dedicated to transforming raw genomic data into accessible, interpretable results. Specialising in building robust bioinformatics pipelines with a focus on streamlined, intuitive UI design. \n Select 'Builds' to see some of my latest work.";
+  const aboutContent = `Hello! My name's Chris, I'm a Computational Biologist dedicated to transforming raw genomic data into accessible, interpretable results. Specialising in building robust bioinformatics pipelines with a focus on streamlined, intuitive UI design.
+  Select 'Builds' to see some of my latest work.`;
 
   const upBtn = document.querySelector("#nav-up");
   const downBtn = document.querySelector("#nav-down");
